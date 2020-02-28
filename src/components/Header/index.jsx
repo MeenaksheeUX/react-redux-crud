fa05f92b994e4b22e7f63a7ffe03208e9a6980b4
Meenakshee Verma <meenaksheeverma.mca@gmail.com>
2020-02-28 13:11:08 -0800

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>React + Redux CRUD Example</h1>
+    </div>
+  );
+};
+
+export default Header;
